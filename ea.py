addd47debb6d6d0a90ccadaaeb0bcfa36b73e1b7
root <nihole>
2018-12-14 18:48:03 +0000

    comments added

--- ea.py
+++ ea.py
@@ -1,13 +1,18 @@
+# factor determines the difference in weight between the width (y) and the depth (x)
+# the larger the factor, the greater the depth affects the result (x will be greater and x less)
+
 factor = 1.5
+
+# List of data
 a = []
 
-# The number of parameters
-par_number = int(raw_input("Please enter parameters number: "))
+# The number of examined topics
+topic_number = int(raw_input("Please enter topics number: "))
 
 # Get the data
-print "Please enter the values one by one\n"
-for i in range(0, par_number):
-    a.append(float(raw_input("%s: " % i)))
+print "Please enter the rating for each topic  one by one\n"
+for i in range(0, topic_number):
+    a.append(float(raw_input("%s: " % i+1)))
 
 # sort list
 a.sort(reverse=True)
@@ -15,11 +20,11 @@ a.sort(reverse=True)
 # summ numbers in the list
 s = sum(a)
 
-# for each possible y calculate the intersection area
+# for each possible y calculate the intersection area (taking into account the factor)
 
 factor_summ = {}
 
-for y in range(1, par_number+1):
+for y in range(1, topic_number+1):
     # x for this y (sum should be the same)
     x = s/(y)
 
@@ -32,15 +37,18 @@ for y in range(1, par_number+1):
         else:
             factor_summ[y] = factor_summ[y] + a[j]**factor
 
-# find y with maximum sum
+# find y with maximum factor_summ
 y_result = max(factor_summ, key=factor_summ.get)
 
 # find x for this y
 x_result = s/y_result
 
+# x result should not be greater then the maximum assessment
 if x_result > a[0]:
     x_result = a[0]
+    # The area should be equal to the sum of all estimates.
     y_result = s/x_result
 
 
-print "x = %s, y = %s" % (int(round(10*x_result/par_number)), int(round(10*y_result/par_number)))
+# We have to use 10 point scale for the output
+print "x = %s, y = %s" % (int(round(10*x_result/topic_number)), int(round(10*y_result/par_number)))
